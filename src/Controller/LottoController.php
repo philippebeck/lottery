268ca865c25e5f1e4cfbde1e4f1458c49d0a3117
Philippe Beck <philippe@philippebeck.net>
2020-12-09 11:14:45 +0100

    Remove setLottoData() to LottoController

--- src/Controller/LottoController.php
+++ src/Controller/LottoController.php
@@ -15,11 +15,6 @@ use Twig\Error\SyntaxError;
 class LottoController extends MainController
 {
     /**
-     * @var array
-     */
-    private $lotto = [];
-
-    /**
      * @return string
      * @throws LoaderError
      * @throws RuntimeError
@@ -27,33 +22,13 @@ class LottoController extends MainController
      */
     public function defaultMethod()
     {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
         $predictions = [];
 
         return $this->render("back/lotto/predictLotto.twig", ["predictions" => $predictions]);
-    }
-
-    private function setLottoData()
-    {
-        $this->lotto["boule_1"] = $this->getPost()->getPostVar("boule_1");
-        $this->lotto["boule_2"] = $this->getPost()->getPostVar("boule_2");
-        $this->lotto["boule_3"] = $this->getPost()->getPostVar("boule_3");
-        $this->lotto["boule_4"] = $this->getPost()->getPostVar("boule_4");
-        $this->lotto["boule_5"] = $this->getPost()->getPostVar("boule_5");
-
-        $this->lotto["seconde_boule_1"] = $this->getPost()->getPostVar("seconde_boule_1");
-        $this->lotto["seconde_boule_2"] = $this->getPost()->getPostVar("seconde_boule_2");
-        $this->lotto["seconde_boule_3"] = $this->getPost()->getPostVar("seconde_boule_3");
-        $this->lotto["seconde_boule_4"] = $this->getPost()->getPostVar("seconde_boule_4");
-        $this->lotto["seconde_boule_5"] = $this->getPost()->getPostVar("seconde_boule_5");
-
-        $this->lotto["numero_chance"]   = $this->getPost()->getPostVar("numero_chance");
-        $this->lotto["jour_de_tirage"]  = $this->getPost()->getPostVar("jour_de_tirage");
-        $this->lotto["date_de_tirage"]  = $this->getPost()->getPostVar("date_de_tirage");
-
-        $this->lotto["annee_numero_de_tirage"]  = $this->getPost()->getPostVar("annee_numero_de_tirage");
-        $this->lotto["combinaison_gagnante"]    = $this->getPost()->getPostVar("combinaison_gagnante");
-
-        $this->lotto["seconde_combinaison_gagnante"] = $this->getPost()->getPostVar("seconde_combinaison_gagnante");
     }
 
     /**
@@ -69,9 +44,7 @@ class LottoController extends MainController
         }
 
         if (!empty($this->getPost()->getPostArray())) {
-            $this->setLottoData();
-
-            ModelFactory::getModel("Lotto")->createData($this->lotto);
+            ModelFactory::getModel("Lotto")->createData($this->getPost()->getPostArray());
             $this->getSession()->createAlert("New lotto created successfully !", "green");
 
             $this->redirect("admin");
@@ -93,9 +66,7 @@ class LottoController extends MainController
         }
 
         if (!empty($this->getPost()->getPostArray())) {
-            $this->setLottoData();
-
-            ModelFactory::getModel("Lotto")->updateData($this->getGet()->getGetVar("id"), $this->lotto);
+            ModelFactory::getModel("Lotto")->updateData($this->getGet()->getGetVar("id"), $this->getPost()->getPostArray());
             $this->getSession()->createAlert("Successful modification of the selected lotto !", "blue");
 
             $this->redirect("admin");
